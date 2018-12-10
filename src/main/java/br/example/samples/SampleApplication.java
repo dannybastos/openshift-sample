eb4480ef620c3aef95ac375ff3f161ba808cfe6e
Danny Bastos <danny.bastos@entelgy.com.br>
2018-12-10 17:40:55 -0200

    initial commit

---
+++ src/main/java/br/example/samples/SampleApplication.java
@@ -0,0 +1,36 @@
+package br.example.samples;
+
+import com.example.Server11;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dannybastos on 10/12/18.
+ */
+
+
+@SpringBootApplication
+@RestController
+public class SampleApplication {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class);
+    }
+
+    @GetMapping("hello")
+    public ResponseEntity<String> sayHello(@RequestParam("name") String name) {
+        Server11 server11 = new Server11();
+        server11.setName(name);
+        log.info(server11.toString());
+        return ResponseEntity.ok(server11.getName()+"\n");
+    }
+
+}
