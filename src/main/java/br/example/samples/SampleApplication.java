0832daf6eabe005dbcad4d26de7914ed7feadec5
Danny Bastos <danny.bastos@entelgy.com.br>
2018-12-10 18:06:51 -0200

    changed from tomcat to wildfly

--- src/main/java/br/example/samples/SampleApplication.java
+++ src/main/java/br/example/samples/SampleApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SampleApplication {
+public class SampleApplication extends SpringBootServletInitializer {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
