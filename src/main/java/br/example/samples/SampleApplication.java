fd1b22dda5e7e4f04a5cfef42a05844049f92d24
Danny Bastos <danny.bastos@entelgy.com.br>
2018-12-11 10:38:40 -0200

    fixed undwetow

--- src/main/java/br/example/samples/SampleApplication.java
+++ src/main/java/br/example/samples/SampleApplication.java
@@ -1,6 +1,5 @@
 package br.example.samples;
 
-import com.example.Server11;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Server11;
 
 /**
  * Created by dannybastos on 10/12/18.
@@ -25,7 +26,7 @@ public class SampleApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class);
     }
-
+    
     @GetMapping("hello")
     public ResponseEntity<String> sayHello(@RequestParam("name") String name) {
         Server11 server11 = new Server11();
